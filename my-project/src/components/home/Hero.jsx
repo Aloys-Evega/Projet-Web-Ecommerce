70d3f7f6da84251cf854b32c7a0047be4706d745
Aloys <aloys.essomba@2028.ucac-icam.com>
2024-10-30 07:25:15 +0100

    Premiere version de ma Hero section

--- my-project/src/components/home/Hero.jsx
+++ my-project/src/components/home/Hero.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div className=" flex justify-between items-center p-4 space-x-8">
+            <div className="section1 basis-1/2">
+                <h1 className='text-center'>Bienvenue dans notre page de delivery</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis qui molestiae officia commodi aperiam ad nostrum mollitia, sequi ratione iure eum molestias nesciunt rem harum eos dolor? Molestias, dolorum. Minima!</p>
+            </div>
+            <div className="section2 basis-1/2">
+                <img src="" alt="image de tchop et Yamo"/>
+            </div>
+        </div>
+    </div>
   )
 }
